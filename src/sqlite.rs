9ac806f77c784dbf17bfbea071d19a46422db3d2
James Go <jamescookgo@gmail.com>
2022-05-23 22:53:07 -0700

    Add migrator for sqlite

--- src/sqlite.rs
+++ src/sqlite.rs
@@ -1,13 +1,13 @@
 use rusqlite::{Connection, params, Row, Rows};
-use anyhow::Result;
+use anyhow::Result as Res;
 use crate::error::AppError;
 
 pub trait DBObject: Sized {
 	fn table_name() -> &'static str;
 	fn get_id(&self) -> i64;
-	fn from_row(row: &Row<'_>) -> Result<Self>;
+	fn from_row(row: &Row<'_>) -> Res<Self>;
 
-	fn load_by_primary_key(cxn: &Connection, id: i64) -> Result<Self> {
+	fn load_by_primary_key(cxn: &Connection, id: i64) -> Res<Self> {
 		let mut stmt = cxn.prepare(&format!(r#"
 			SELECT *
 			FROM {}
@@ -21,7 +21,7 @@ pub trait DBObject: Sized {
 		}
 	}
 
-	fn from_single_row(rows: &mut Rows<'_>) -> Result<Option<Self>> {
+	fn from_single_row(rows: &mut Rows<'_>) -> Res<Option<Self>> {
 		if let Some(row) = rows.next()? {
 			Ok(Some(Self::from_row(row)?))
 		} else {
@@ -29,11 +29,93 @@ pub trait DBObject: Sized {
 		}
 	}
 
-	fn from_multiple_rows(rows: &mut Rows<'_>) -> Result<Vec<Self>> {
+	fn from_multiple_rows(rows: &mut Rows<'_>) -> Res<Vec<Self>> {
 		let mut results = Vec::new();
 		while let Some(row) = rows.next()? {
 			results.push(Self::from_row(row)?);
 		}
 		Ok(results)
+	}
+}
+
+pub struct Migrator {
+	migrations: Vec<Migration>,
+}
+
+impl Migrator {
+	pub fn new() -> Self {
+		Self {
+			migrations: Vec::new(),
+		}
+	}
+
+	pub fn add_migration(&mut self, migration: Migration) -> Res<()> {
+		if self.migrations.iter().find(|x| x.version == migration.version).is_some() {
+			return Err(AppError::new("Two migrations with the same version are not allowed").into());
+		}
+
+		self.migrations.push(migration);
+
+		Ok(())
+	}
+
+	pub fn run_migrations(&mut self, cxn: &Connection) -> Res<()> {
+		self.migrations.sort_by(|a, b| a.version.cmp(&b.version));
+
+		for migration in &self.migrations {
+			migration.run(cxn)?;
+		}
+
+		Ok(())
+	}
+}
+
+pub struct Migration {
+	version: u32,
+	statements: Vec<String>,
+}
+
+impl Migration {
+	pub fn new(version: u32, statements: Vec<String>) -> Self {
+		Self {
+			version,
+			statements,
+		}
+	}
+
+	fn run(&self, cxn: &Connection) -> Res<()> {
+		if !self.should_run_migration(cxn)? {
+			return Ok(());
+		}
+
+		self.migrate(cxn)?;
+
+		self.set_migration_version(cxn)?;
+		Ok(())
+	}
+
+	fn should_run_migration(&self, cxn: &Connection) -> Res<bool> {
+		let mut stmt = cxn.prepare("PRAGMA user_version")?;
+		let mut rows = stmt.query(params![])?;
+		if let Some(row) = rows.next()? {
+			let db_version: u32 = row.get("user_version")?;
+			Ok(db_version < self.version)
+		} else {
+			Ok(true)
+		}
+	}
+
+	fn set_migration_version(&self, cxn: &Connection) -> Res<()> {
+		let mut stmt = cxn.prepare(&format!("PRAGMA user_version = {}", self.version))?;
+		stmt.execute(params![])?;
+		Ok(())
+	}
+
+	fn migrate(&self, cxn: &Connection) -> Res<()> {
+		for stmt in &self.statements {
+			let mut stmt = cxn.prepare(&stmt)?;
+			stmt.execute(params![])?;
+		}
+		Ok(())
 	}
 }
