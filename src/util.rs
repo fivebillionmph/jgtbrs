bad14efcbde0519a676a0f5b958a9096e1bbd4be
James Go <jamescookgo@gmail.com>
2022-06-27 08:38:02 -0700

    Convert path to string

--- src/util.rs
+++ src/util.rs
@@ -1,6 +1,12 @@
+use std::path::Path;
 use anyhow::Result as Res;
+use crate::error::AppError;
 
 pub fn get_unix_timestamp() -> Res<u64> {
 	use std::time::SystemTime;
 	Ok(SystemTime::now().duration_since(SystemTime::UNIX_EPOCH)?.as_secs())
+}
+
+pub fn path_to_string(path: &Path) -> Res<String> {
+	Ok(path.as_os_str().to_str().ok_or(AppError::new("Invalid path name"))?.to_string())
 }
